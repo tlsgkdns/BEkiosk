83e0fe432389a0096e9feacd7880d65f83c6886b
tlsgkdns <tlsgkdns@gmail.com>
2023-12-13 17:03:28 +0900

    LV4 Complete

---
+++ src/main/kotlin/Cart.kt
@@ -0,0 +1,34 @@
+class Cart {
+    private var cart = mutableMapOf<String, Int>()
+    private var cost = 0
+    fun addProduct(menu: Menu, count: Int)
+    {
+        val key = menu.getName()
+        cart[key] = count
+        cost += menu.getPrice() * count
+    }
+    fun payProducts(money: Int): Int
+    {
+        if(cost > money)
+        {
+            println("${cost - money}원 부족합니다. 돈을 더 넣어주세요.")
+            return money
+        }else
+        {
+            val remain = money - cost
+            println("${cost}원 결제되었습니다.")
+            println("남은 금액은 ${remain}원입니다.")
+            cart = mutableMapOf<String, Int>()
+            cost = 0
+            return remain
+        }
+    }
+
+    fun displayCartInfo()
+    {
+        println("[장바구니]")
+        for(product in cart)
+            println("${product.key}: ${product.value}개")
+        println("총 ${cost}원입니다.")
+    }
+}
